96fc0fad41733b7690c57a313dd6e2e225fee744
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2026-02-11 15:46:51 +0200

    Added "echo" command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // loop
+        Scanner scanner = new Scanner(System.in);
         while(true) {
-            // read
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            // eval
-            if (input.equals("exit")) {
-                break;
-            }
-            // print
-            System.out.println(input + ": command not found");
 
+            String[] line = input.split(" ", 2);
+            String command = line[0];
+
+            String arguments = " ";
+            if(line.length > 1) {
+                arguments = line[1];
+            }
+
+            switch (command) {
+                case "exit" -> exit();
+                case "echo" -> echo(arguments);
+                default -> notFound(command);
+            }
         }
+
+    }
+
+    private static void exit() {
+        System.exit(0);
+    }
+
+    private static void echo(String text) {
+        System.out.println(text);
+    }
+
+    private static void notFound(String input) {
+        System.out.println(input + ": command not found");
     }
 
 }
