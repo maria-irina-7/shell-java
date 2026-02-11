7d0ab48d3c53e83f930d713b104e838577fad9b8
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2026-02-11 15:17:52 +0200

    Added "exit" command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // loop
         while(true) {
+            // read
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             // eval
-
+            if (input.equals("exit")) {
+                break;
+            }
             // print
             System.out.println(input + ": command not found");
+
         }
     }
 
