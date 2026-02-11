7cc66a0038ae6980f1177713caa879c135890003
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2026-02-11 15:15:06 +0200

    Infinite loop when running commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        while(true) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            // eval
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+            // print
+            System.out.println(input + ": command not found");
+        }
     }
+
 }
