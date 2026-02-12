c6fe9d395fa5a132c65a86dcad4e10b8b52cdfa9
Maria Irina Niculescu <maria.irina.niculescu@gmail.com>
2026-02-12 14:07:07 +0200

    Changed to an OS-agnostic commands split

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -15,7 +15,7 @@ public class Commands {
 
     public static String getOutsideCommand(String name) {
         String allOutsideCommands = System.getenv("PATH");
-        String[] pathCommands = allOutsideCommands.split(":");
+        String[] pathCommands = allOutsideCommands.split(File.pathSeparator);
 
         for(int i = 0; i < pathCommands.length; i++) {
             File file = new File(pathCommands[i], name);
